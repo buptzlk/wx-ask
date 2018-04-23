fe34ccf31d167cec0c5f9f276bdff394b330b606
dengguochuan <dengguochuan@baidu.com>
2018-04-24 00:02:22 +0800

    答题页面和成绩页面

--- pages/answer/answer.js
+++ pages/answer/answer.js
@@ -1,30 +1,63 @@
 // pages/answer/answer.js
+var index = 0;
+var count = 2;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    title:'',
+    A:'A',
+    B:'B',
+    C:'C',
+    D:'D'
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // question: wx.getStorage({
-    //   key: 'question',
-    //   success: function(res) {
-    //     console.log('onLoadok');
-    //     that.setData({
-    //       items: res.data
-    //     });
-    //     console.log(res)
-    //   },
-    // });
-        this.setData({items: wx.getStorageSync('question')})
+    console.log('index   :'+index);
+    if (wx.getStorageSync('index') > 0) {
+      console.log('>0' );
+      index = wx.getStorageSync('index');
+    } 
+    if (index > count || index == count) {
+      index = 0;
+    }
+    var items = wx.getStorageSync('question')
+    var options = items[index].options
+    var obj = JSON.parse(options)
+   
+    this.setData({
+      title: items[index].title,
+      answer: items[index].result,
+      A: obj.A,
+      B: obj.B,
+      C: obj.C,
+      D: obj.D,
+      num:index+1
+    });   
   },
-
+  answerNext: function () {
+    console.log('index dianji hou :'+index);
+    if (index + 1 < count) {
+      wx.setStorageSync('index', index + 1);
+      wx.redirectTo({
+        url: '../answer/answer'
+      })
+    } else {
+      index = 0;
+      wx.removeStorageSync('index');
+      console.log(wx.getStorageSync('index'))
+      wx.redirectTo({
+        url: '../score/score',
+      })
+    }
+   
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -43,7 +76,7 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+    console.log("hide");
   },
 
   /**
