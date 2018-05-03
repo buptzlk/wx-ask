0f2d58f5b3e6928e5d03dcac7da8bcea548c7afa
dengguochuan <dengguochuan@baidu.com>
2018-05-03 18:51:43 +0800

    积分榜

--- pages/cinema/index.js
+++ pages/cinema/index.js
@@ -1,10 +1,18 @@
+var app = getApp()
 Page({
     data: {
         title: ''
     },
     //事件处理函数
     onLoad: function (options) {
-        var that = this
+      var that = this
+      //调用应用实例的方法获取全局数据
+      app.getUserInfo(function (userInfo) {
+        //更新数据
+        that.setData({
+          userInfo: userInfo
+        })
+      })
 
         //playingList
         wx.request({
